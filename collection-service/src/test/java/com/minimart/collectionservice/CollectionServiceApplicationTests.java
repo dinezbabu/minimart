8465a6c73911b3c1f8fe577e3de3ea77097fd88c
BABU, DINESH <dbabu@mast.com>
2019-04-29 02:34:37 +0530

    Basic Service Added

---
+++ collection-service/src/test/java/com/minimart/collectionservice/CollectionServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.minimart.collectionservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CollectionServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
